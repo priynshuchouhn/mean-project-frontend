6dc9d0cd224b7e5f78ba6b881f71a7a397032ef1
Priyanshu Chouhan <priyanschouhan123@gmail.com>
2023-11-20 22:48:27 +0530

    API URL updated

--- src/app/shared/services/API.ts
+++ src/app/shared/services/API.ts
@@ -1,5 +1,6 @@
 export class API {
-    public static baseURL = 'http://localhost:3000/api/';
+    public static baseURL = 'https://daily-blog-zbnj.onrender.com/api/';
+    // public static baseURL = 'http://localhost:3000/api/';
 
     public static getAllPost = this.baseURL +'posts';
     public static getPost = this.baseURL +'posts/id/';
